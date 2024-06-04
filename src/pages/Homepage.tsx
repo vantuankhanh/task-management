3e4647642d867eb3cb62af323fe849856ae9d860
Van Tuan Khanh <vantuankhanh09@gmail.com>
2024-06-05 03:53:22 +0700

    init message page

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,6 +1,6 @@
 const Homepage = () => {
   return (
-    <div className="min-h-96 box-center bg-slate-100">
+    <div className="flex-1 box-center bg-slate-100">
       <h1 className="text-4xl">
         Welcome to <span className="text-blue-600">Task Manager</span> website
       </h1>
